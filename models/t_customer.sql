c1906435be4cf8d35d992d3d9d933c3a9406b9c9
Dev More <deomore23@gmail.com>
2026-05-16 10:53:30 +0000

    NEW_MODEL

--- models/t_customer.sql
+++ models/t_customer.sql
@@ -1,6 +1,3 @@
 SELECT 
-    CUSTOMER_ID,
-    USERNAME,
-    PASSWORD AS customer_password
-FROM {{ source('s1', 'customer') }}
-WHERE CUSTOMER_ID = 1
+    *
+FROM {{ source('s1', 't_customers') }}
