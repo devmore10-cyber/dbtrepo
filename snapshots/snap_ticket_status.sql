cc8b751468bda6bbcda93959413946a91e4c4afb
Dev More <deomore23@gmail.com>
2026-05-19 03:00:48 +0000

    commit

--- snapshots/snap_ticket_status.sql
+++ snapshots/snap_ticket_status.sql
@@ -1,14 +1,15 @@
 {% snapshot snapshot_name %}
-    {{
-        config(
-            target_schema='DBT_SCHEMA',
-            target_database='SBI_DB',
-            unique_key='field',
-            strategy='timestamp',
-            invalidate_hard_deletes=False,
-            updated_at='updated_at_field'
-        )
-    }}
 
-    select * from {{ source('S1', 'm_ticket_status') }}
- {% endsnapshot %}
+{{
+    config(
+        schema='SBI_SCHEMA',
+        database='SBI_DB',
+        unique_key='id',
+        strategy='check',
+        check_cols=['status']
+    )
+}}
+
+select * from {{ ref('m_ticket_status') }}
+
+{% endsnapshot %}
